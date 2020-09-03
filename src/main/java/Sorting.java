8ed41965fd4d9c5bc11f7827368057aca9908506
andervyd <andervyd@gmail.com>
2020-09-03 15:08:08 +0300

    add Sorting class

---
+++ src/main/java/Sorting.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Sorting {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Alex");
+		list.add("Zaur");
+		list.add("Ivan");
+		list.add("Petr");
+		
+		System.out.println("Before sorting: ");
+		System.out.println(list);
+		
+		Collections.sort(list);
+		
+		System.out.println("After sorting: ");
+		System.out.println(list);
+		
+	}
+}

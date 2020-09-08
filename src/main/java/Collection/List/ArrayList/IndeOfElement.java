1e252c314777d86aeac71d5b0bee4f73ffb8bbbe
andervyd <andervyd@gmail.com>
2020-09-08 08:14:45 +0300

    add some methods

---
+++ src/main/java/Collection/List/ArrayList/IndeOfElement.java
@@ -0,0 +1,81 @@
+package Collection.List.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+
+
+class Employee {
+
+    private String name;
+    private int age;
+    private int course;
+
+    public Employee(String name, int age, int course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name: " + name
+                + ", age: " + age
+                + ", course: " + course + "]"
+                + "\n";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+
+        Employee employee = (Employee) object;
+        return age == employee.age &&
+                course == employee.course &&
+                Objects.equals(name, employee.name);
+    }
+}
+
+
+public class IndeOfElement {
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee("Mary", 23, 3);
+        Employee emp2 = new Employee("Petr", 21, 1);
+        Employee emp3 = new Employee("Oleg" , 24, 5);
+        Employee emp4 = new Employee("Sveta", 22, 2);
+        Employee emp5 = new Employee("Sasha", 24, 4);
+
+        List<Employee> listEmployee = new ArrayList<>();
+
+        listEmployee.add(emp1);
+        listEmployee.add(emp2);
+        listEmployee.add(emp3);
+        listEmployee.add(emp4);
+        listEmployee.add(emp5);
+
+        System.out.println(listEmployee);
+
+        Employee emp6 = new Employee("Sasha", 24, 4);
+
+        // In the presence of щverride equals (!)
+        System.out.println("\n" + "Index emp6: " + listEmployee.indexOf(emp6));
+
+
+
+    }
+}/* Output:
+            [Student [name: Mary, age: 23, course: 3]
+            , Student [name: Petr, age: 21, course: 1]
+            , Student [name: Oleg, age: 24, course: 5]
+            , Student [name: Sveta, age: 22, course: 2]
+            , Student [name: Sasha, age: 24, course: 4]
+            ]
+
+            Index emp6: 4
+*///:~
--- src/main/java/Collection/List/ArrayList/MethodsArrayList.java
+++ src/main/java/Collection/List/ArrayList/MethodsArrayList.java
@@ -9,7 +9,7 @@ public class MethodsArrayList {
 		
 		System.out.println("-----------------------");
 		
-		// add(Data Type element) => boolean
+// add(Data Type element) => boolean
 		
 		ArrayList<String> addList = new ArrayList<>();
 		addList.add("Mary");
@@ -19,40 +19,40 @@ public class MethodsArrayList {
 		for(String string : addList) {
 			System.out.print(string + " ");
 		}
-// Output: Mary Petr Sveta 
+			// Output: Mary Petr Sveta
 		
 		System.out.println("\n" + "-----------------------");
 		
-		// add(int index, Data Type element) => boolean
+// add(int index, Data Type element) => boolean
 		
 		ArrayList<String> addListIndex = new ArrayList<>();
 		addListIndex.add("Mary");
 		addListIndex.add("Petr");
 		addListIndex.add("Sveta");
 		addListIndex.add(1, "Oleg");
-//		addListIndex.add(7, "Oleg"); // Exception: IndexOutOfBoundsException: Index: 7, Size: 4
+      //addListIndex.add(7, "Oleg"); // Exception: IndexOutOfBoundsException: Index: 7, Size: 4
 		
 		for(String string : addListIndex) {
 			System.out.print(string + " ");
 		}
-// Output: Mary Oleg Petr Sveta
+			// Output: Mary Oleg Petr Sveta
 		
 		System.out.println("\n" + "-----------------------");
 
-		// get(int index) => Data Type
+// get(int index) => Data Type
 		
 		System.out.println(addListIndex.get(1));
 		
-// Output: Oleg
+			// Output: Oleg
 			
 		for(int i = 0; i < addListIndex.size(); i++) {
 			System.out.print(addListIndex.get(i) + " ");
 		}
-// Output: Mary Oleg Petr Sveta
+			// Output: Mary Oleg Petr Sveta
 		
 		System.out.println("\n" + "-----------------------");
 		
-		// set(int index, Data Type element) => Data Type
+// set(int index, Data Type element) => Data Type
 		
 		System.out.println("Before: " + addListIndex.get(0));
 		
@@ -60,11 +60,11 @@ public class MethodsArrayList {
 		
 		System.out.println("After: " + addListIndex.get(0));
 
-// Output: Before: Mary ||	After: Masha
+			// Output: Before: Mary ||	After: Masha
 		
 		System.out.println("\n" + "-----------------------");
 
-		// remove(Object element) => boolean
+// remove(Object element) => boolean
 		
 		addListIndex.add("Kiril");
 		addListIndex.add("Max");
@@ -72,7 +72,7 @@ public class MethodsArrayList {
 		for(String string : addListIndex) {
 			System.out.print(string + " ");
 		}
-// Output: Masha Oleg Petr Sveta Kiril Max		
+			// Output: Masha Oleg Petr Sveta Kiril Max
 		
 		addListIndex.remove("Max");
 		System.out.println();
@@ -80,28 +80,87 @@ public class MethodsArrayList {
 		for(String string : addListIndex) {
 			System.out.print(string + " ");
 		}
-// Output: Masha Oleg Petr Sveta Kiril		
+			// Output: Masha Oleg Petr Sveta Kiril
 		
-		// remove(int index) => boolean
+// remove(int index) => boolean
 		// addListIndex: [ Masha(0), Oleg(1), Petr(2), Sveta(3), Kiril(4) ]
 		
-		addListIndex.remove(1);		
-//		addListIndex.remove(8);	 // Exception: IndexOutOfBoundsException: Index 8 out of bounds for length 4
+		addListIndex.remove(1);
+	  //addListIndex.remove(8);	 // Exception: IndexOutOfBoundsException: Index 8 out of bounds for length 4
 		
 		System.out.println();
 		
 		for(String string : addListIndex) {
 			System.out.print(string + " ");
 		}
-// Output: Masha Petr Sveta Kiril		
-		
-		
-		
-		
-		
+			// Output: Masha Petr Sveta Kiril
+
 		System.out.println("\n" + "-----------------------");
+
+// addAll(ArrayList arrayList) => boolean
+
+		ArrayList<Integer> listInt1 = new ArrayList<>();
+
+		listInt1.add(1);
+		listInt1.add(2);
+		listInt1.add(3);
+
+		ArrayList<Integer> listInt2 = new ArrayList<>();
+
+		listInt2.add(4);
+		listInt2.add(5);
+		listInt2.add(6);
+
+		listInt1.addAll(listInt2);
+
+		for (Integer i : listInt1) {
+			System.out.print(i + " ");
+		}
+			// Output: 1 2 3 4 5 6
+
 		System.out.println("\n" + "-----------------------");
+
+// addAll(int index, ArrayList arrayList) => boolean
+
+		ArrayList<Integer> listInt3 = new ArrayList<>();
+
+		listInt3.add(10);
+		listInt3.add(11);
+		listInt3.add(12);
+
+		listInt1.addAll(0, listInt3);
+
+		System.out.println(listInt1);
+
+			// Output: [10, 11, 12, 1, 2, 3, 4, 5, 6]
+
 		System.out.println("\n" + "-----------------------");
+
+// clear() => void
+
+		System.out.println("Before clear: " + listInt3);
+
+		listInt3.clear();
+
+		System.out.println("After clear: " + listInt3);
+
+			// Output: Before clear: [10, 11, 12]
+		    //         After clear: []
+
+		System.out.println("\n" + "-----------------------");
+
+// indexOf(Object element) => int (return index element if not -1)
+
+		// listInt2: 4(0), 5(1), 6(2)
+
+		System.out.println(listInt2.indexOf(4));
+			// Output: 0
+
+
+
+
+
+
 		System.out.println("\n" + "-----------------------");
 		System.out.println("\n" + "-----------------------");
 		System.out.println("\n" + "-----------------------");
